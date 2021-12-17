b971ae349391b1255a1960f3c22e494906e3ea9b
andpalmier <andpalmier@gmail.com>
2021-12-17 16:54:42 +0100

    fix reference and go.mod

--- cmd/gocphish.go
+++ cmd/gocphish.go
@@ -1,118 +1,118 @@
 package main
 
 import (
-    "flag"
-    "fmt"
-    "github.com/CaliDog/certstream-go"
-    "github.com/texttheater/golang-levenshtein/levenshtein"
-    "strings"
+	"flag"
+	"fmt"
+	"github.com/CaliDog/certstream-go"
+	"github.com/texttheater/golang-levenshtein/levenshtein"
+	"strings"
 )
 
 var (
-    // already visited domains
-    domainsSet = make(map[string]bool)
+	// already visited domains
+	domainsSet = make(map[string]bool)
 )
 
 func main() {
 
-    // specify config file with c
-    configFile := flag.String("c", "config.json", "config file")
-    flag.Parse()
+	// specify config file with c
+	configFile := flag.String("c", "config.json", "config file")
+	flag.Parse()
 
-    // parse config file
-    conf := ParseConfig(*configFile)
-    // from documentation of certstream-go
-    stream, errStream := certstream.CertStreamEventStream(true)
+	// parse config file
+	conf := ParseConfig(*configFile)
+	// from documentation of certstream-go
+	stream, errStream := certstream.CertStreamEventStream(true)
 
-    for {
-	select {
-	case jq := <-stream:
-	    // get all domains event
-	    domains, err := jq.ArrayOfStrings("data", "leaf_cert", "all_domains")
-	    if err != nil {
-		panic(err)
-	    }
+	for {
+		select {
+		case jq := <-stream:
+			// get all domains event
+			domains, err := jq.ArrayOfStrings("data", "leaf_cert", "all_domains")
+			if err != nil {
+				panic(err)
+			}
 
-	    // parse every domain received
-	    for _, domain := range domains {
-		dom, res := CheckDomain(domain, conf)
-		if res > 0 {
-		    fmt.Printf("%s [score: %d]\n",dom,res)
-		    //fmt.Printf("%s\n", dom)
+			// parse every domain received
+			for _, domain := range domains {
+				dom, res := CheckDomain(domain, conf)
+				if res > 0 {
+					fmt.Printf("%s [score: %d]\n", dom, res)
+					//fmt.Printf("%s\n", dom)
+				}
+			}
+
+		case _ = <-errStream:
+			continue
 		}
-	    }
-
-	case _ = <-errStream:
-	    continue
 	}
-    }
 
 }
 
 // CheckDomain callback function when a newly registered domain is found
 func CheckDomain(domain string, conf Config) (string, int) {
-    var dom string
+	var dom string
 
-    // normalize text
-    dom = NormalizeText(domain)
+	// normalize text
+	dom = NormalizeText(domain)
 
-    // remove prefixes which cause duplicates
-    for _, remove := range conf.ToRemove {
-	if strings.Contains(dom, remove) {
-	    dom = strings.TrimPrefix(dom, remove)
-	}
-    }
-
-    // check if already visited
-    if !domainsSet[dom] {
-
-	// add to the visited set
-	domainsSet[dom] = true
-
-	// suspiciousness of the domain
-	urlval := 0
-
-	// loop in suspicious TLDs
-	for _, tld := range conf.SuspiciousTlds {
-
-	    // check if suspicious tld
-	    if strings.HasSuffix(dom, tld) {
-		urlval += conf.SuspiciousTldsVal
-	    }
+	// remove prefixes which cause duplicates
+	for _, remove := range conf.ToRemove {
+		if strings.Contains(dom, remove) {
+			dom = strings.TrimPrefix(dom, remove)
+		}
 	}
 
-	// Split domain in array
-	domarray := strings.Split(dom, ".")
+	// check if already visited
+	if !domainsSet[dom] {
 
-	// loop in the domain array
-	for _, partialdomain := range domarray {
+		// add to the visited set
+		domainsSet[dom] = true
 
-	    // loop suspicious keywords
-	    for keyword, value := range conf.Keywords {
+		// suspiciousness of the domain
+		urlval := 0
 
-		// if suspicious keyword is contained in the
-		// element of the domain array, increment the
-		// suspiciousness value of the domain with the
-		// corresponding value of the keyword
-		if strings.Contains(partialdomain, keyword) {
-		    urlval += value
+		// loop in suspicious TLDs
+		for _, tld := range conf.SuspiciousTlds {
+
+			// check if suspicious tld
+			if strings.HasSuffix(dom, tld) {
+				urlval += conf.SuspiciousTldsVal
+			}
 		}
 
-		// check for similar keywords with levenshtein
-		// distance if the value of the keyword is > 60
-		if value >= 60 {
-		    distance := levenshtein.DistanceForStrings([]rune(keyword), []rune(partialdomain), levenshtein.DefaultOptions)
-		    if distance == 1 {
-			urlval += 50
-		    }
-		}
-	    }
-	}
+		// Split domain in array
+		domarray := strings.Split(dom, ".")
 
-	// return suspicious domains
-	if urlval > conf.SuspiciousThreshold {
-	    return dom, urlval
+		// loop in the domain array
+		for _, partialdomain := range domarray {
+
+			// loop suspicious keywords
+			for keyword, value := range conf.Keywords {
+
+				// if suspicious keyword is contained in the
+				// element of the domain array, increment the
+				// suspiciousness value of the domain with the
+				// corresponding value of the keyword
+				if strings.Contains(partialdomain, keyword) {
+					urlval += value
+				}
+
+				// check for similar keywords with levenshtein
+				// distance if the value of the keyword is > 60
+				if value >= 60 {
+					distance := levenshtein.DistanceForStrings([]rune(keyword), []rune(partialdomain), levenshtein.DefaultOptions)
+					if distance == 1 {
+						urlval += 50
+					}
+				}
+			}
+		}
+
+		// return suspicious domains
+		if urlval > conf.SuspiciousThreshold {
+			return dom, urlval
+		}
 	}
-    }
-    return "", 0
+	return "", 0
 }
--- go.mod
+++ go.mod
@@ -1,12 +1,15 @@
-module gocatchphish
+module github.com/andpalmier/gocatchphish
 
-go 1.15
+go 1.17
 
 require (
-	github.com/CaliDog/certstream-go v0.0.0-20200713031452-eca7997412f1 // indirect
+	github.com/CaliDog/certstream-go v0.0.0-20200713031452-eca7997412f1
+	github.com/texttheater/golang-levenshtein/levenshtein v0.0.0-20200805054039-cae8b0eaed6c
+	golang.org/x/text v0.3.7
+)
+
+require (
 	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/jmoiron/jsonq v0.0.0-20150511023944-e874b168d07e // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	github.com/texttheater/golang-levenshtein/levenshtein v0.0.0-20200805054039-cae8b0eaed6c // indirect
-	golang.org/x/text v0.3.3
 )
